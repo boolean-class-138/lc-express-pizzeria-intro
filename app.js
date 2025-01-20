0e0f4407688094a93f48f13e127fa7288370cf58
samuelemadrigali <samuele.madrigali@gmail.com>
2025-01-20 12:37:17 +0100

    feat: add pizze

--- app.js
+++ app.js
@@ -11,9 +11,29 @@ app.get('/', (req, res) => {
 app.get('/menu', (req, res) => {
     res.json([
         {
-            name: 'Margherita',
-            image: `http://localhost:${port}/pizze/margherita.webp`,
-            ingredients: ['pomodoro', 'mozzarella']
+          name: 'Margherita',
+          image: `http://localhost:${port}/pizze/margherita.webp`,
+          ingredients: ['pomodoro', 'mozzarella']
+        },
+        {
+          name: "Marinara",
+          image: `http://localhost:${port}/pizze/marinara.jpeg`,
+          ingredients: ["pomodoro", "aglio", "origano"],
+        },
+        {
+          name: "Diavola",
+          image: `http://localhost:${port}/pizze/diavola.jpeg`,
+          ingredients: ["pomodoro", "mozzarella", "salame piccante"],
+        },
+        {
+          name: "Bufalina",
+          image: `http://localhost:${port}/pizze/bufalina.jpeg`,
+          ingredients: ["pomodoro", "mozzarella di bufala"],
+        },
+        {
+          name: "4 formaggi",
+          image: `http://localhost:${port}/pizze/4_formaggi.jpeg`,
+          ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
         }
     ])
 })
