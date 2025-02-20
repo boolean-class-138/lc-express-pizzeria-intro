6c4883e70671b8d7b66c77dd724ae275ab837732
samuelemadrigali <samuele.madrigali@gmail.com>
2025-02-20 08:20:28 +0100

    feat: add cors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 3001;
@@ -7,6 +8,7 @@ const employersRouter = require("./routers/employersRouter");
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server della mia pizzeria");
--- data/pizzasData.js
+++ data/pizzasData.js
@@ -1,34 +1,50 @@
 const pizzas = [
   {
     id: 1,
-    name: 'Margherita',
+    name: "Margherita",
     image: `http://localhost:3001/pizze/margherita.webp`,
-    ingredients: ['pomodoro', 'mozzarella']
+    ingredients: ["pomodoro", "mozzarella"],
+    description:
+      "La classica pizza napoletana con pomodoro fresco e mozzarella, simbolo della tradizione italiana.",
   },
   {
     id: 2,
     name: "Marinara",
     image: `http://localhost:3001/pizze/marinara.jpeg`,
     ingredients: ["pomodoro", "aglio", "origano"],
+    description:
+      "Una pizza semplice ma gustosa, con pomodoro, aglio e origano. Un piacere per gli amanti dei sapori autentici.",
   },
   {
     id: 3,
     name: "Diavola",
     image: `http://localhost:3001/pizze/diavola.jpeg`,
     ingredients: ["pomodoro", "mozzarella", "salame piccante"],
+    description:
+      "Pizza dal sapore deciso, con pomodoro, mozzarella e il tocco piccante del salame. Per chi ama il gusto forte.",
   },
   {
     id: 4,
     name: "Bufalina",
     image: `http://localhost:3001/pizze/bufalina.jpeg`,
     ingredients: ["pomodoro", "mozzarella di bufala"],
+    description:
+      "Una pizza prelibata con pomodoro fresco e mozzarella di bufala, per un sapore cremoso e ricco.",
   },
   {
     id: 5,
     name: "4 formaggi",
     image: `http://localhost:3001/pizze/4_formaggi.jpeg`,
-    ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
-  }
+    ingredients: [
+      "pomodoro",
+      "mozzarella",
+      "gorgonzola",
+      "parmigiano",
+      "ricotta",
+    ],
+    description:
+      "Una combinazione di quattro formaggi che si fondono sulla pizza, con un mix di sapori delicati e intensi.",
+  },
 ];
 
-module.exports = pizzas
+module.exports = pizzas;
